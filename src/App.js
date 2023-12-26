458525200a9541e02846e3cf75ae09d81bbddda4
Vikas <vikas.bale834@gmail.com>
2023-12-26 12:02:38 +0530

    update..

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import Header from "./components/Header";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/Home.scss";
-import "./styles/Contact.scss"
+import "./styles/Contact.scss";
 import "./styles/Footer.scss";
 import "./styles/mediaquerry.scss";
-// import About from './components/About';
-import Contact from './components/Contact';
-import Services from './components/Services';
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
 import Footer from "./components/Footer";
 
-
-
-function App(){
-  return(
-    
+function App() {
+  return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/Contact" element={<Contact />}/>
-        <Route path="/Services" element={<Services />}/>
-       
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
-    
   );
 }
 export default App;
